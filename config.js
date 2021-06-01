49e9e532a0d26671fa9db2bf6ded15d788ababd7
LordFader Pessoa <lordfader.pessoa@gmail.com>
2021-06-01 21:07:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ var settings = {
     'radio_name': 'SoundWorks',
 
     // URL of streaming without / on the final, eg: http://streaming.com:8080
-    'url_streaming': 'https://soundworks.live:8000',
-	'stream_path': '/128kbps.mp3',
+    'url_streaming': 'https://soundworks.live:8000/128kbps.mp3',
+	//'stream_path': '/128kbps.mp3',
 
     // When choosing icecast, make sure the file 'player.log' in the document root is writeable.
     // Choose between 'shoutcast' or 'icecast'
